8152eb15e4da49b18bc808f283932c20ede1de1a
emanuelle <emanuelle.sanntoslima@gmail.com>
2019-10-02 17:38:00 -0300

    Adicionar Sistema

---
+++ script.js
@@ -0,0 +1,33 @@
+let nome = prompt("Qual é o seu nome ?");
+let idade = prompt("Qual é a sua idade?");
+let salario = prompt("Qual é o seu salario?");
+let emprestimo =prompt("Qual valor do emprestimo?");
+let parcelas = prompt("Deseja Parcelar?");
+
+    if (idade >= 25 && <=60)
+    let salario = prompt("Digite o seu salario mensal"));
+       if (salario >=1500)
+          let emprestimo = prompt("Digite o valor do emprestimo"));
+            if (emprestimo >= 10* )
+                let parcelas = prompt("Digite quantas parcelas você deseja fazer?"));
+               if (parcelas <= idade){
+                alert("Emprestimo Aprovado")
+               }else{
+                   alert("Emprestimo aprovado");
+            }else{
+              alert("Emprestimo negado pela parcelas")
+        }else{
+            alert("Emprestimo negado pelo valor")
+     }else{
+      alert("Emprestimo negado pelo salario")
+  }else{
+      alert("Empresitimo negado pela idade")
+  }
+     
+    
+            
+
+
+
+
+}
